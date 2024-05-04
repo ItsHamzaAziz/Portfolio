cb943cb6d77dbc9b8c9787834ffad1882a735458
Hamza <hamzaaziz086@gmail.com>
2024-05-04 14:08:41 +0500

    added all files

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMobileNav() {
+    let mobNav = document.getElementById("mobile-nav");
+    if (mobNav.style.display === "block") {
+        mobNav.style.display = "none";
+    } else {
+        mobNav.style.display = "block";
+    }
+}
+
+
